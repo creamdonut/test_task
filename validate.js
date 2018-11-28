ebce598f5d293b9665c90f1f28e0d6e9110f18b1
creamdonut <mrretoy@gmail.com>
2018-11-28 20:38:31 +0500

    Add files via upload

--- validate.js
+++ validate.js
@@ -27,8 +27,8 @@ $( document ).ready(function(){
 							  $( "form" ).submit(function( event3 ){ 
 							    event3.preventDefault(); 
 							   if ($('input[name=check]:checked').val() == "1" && document.getElementById('amount2').value < "1000") 
-							   alert('Пожалуйста, введите сумму от 1 000 до 3 000 000');
+							   alert('Пожалуйста, введите сумму пополнения от 1 000 до 3 000 000');
 								if ($('input[name=check]:checked').val() == "1" && document.getElementById('amount2').value > "3000000") 
-							   alert('Пожалуйста, введите сумму от 1 000 до 3 000 000');
+							   alert('Пожалуйста, введите сумму пополнения от 1 000 до 3 000 000');
 							  });
 							});
